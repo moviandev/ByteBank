c69c198d3da11aca035918b8f4bde59be90a2031
Matheus Viana <matheus_o_viana@hotmail.com>
2021-01-14 22:54:50 -0300

    removed unused import

--- ByteBank/Funcionarios/Desenvolvedor.cs
+++ ByteBank/Funcionarios/Desenvolvedor.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ByteBank.Funcionarios
+﻿namespace ByteBank.Funcionarios
 {
     class Desenvolvedor : Funcionario
     {
