c547d1dac68c573b85ddbce0d7b9b4d3ee1a95a3
Matheus Viana <matheus_o_viana@hotmail.com>
2021-02-28 17:58:33 -0300

    added array logic

---
+++ ByteBank.SistemaAgencia/ListaContaCorrente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ListaContaCorrente 
+    {
+        private ContaCorrente[] _items;
+        private int _nextItem;
+
+        public ListaContaCorrente()
+        {
+            _items = new ContaCorrente[5];
+            _nextItem = 0;
+        }
+
+        public void Adicionar(ContaCorrente item)
+        {
+            Console.WriteLine($"Adding item int at the position {_nextItem}");
+            _items[_nextItem] = item;
+            _nextItem++;
+        }
+
+        private void CheckCapacity(int capacity)
+        {
+            if (_items.Length >= capacity)
+                return;
+
+
+            ContaCorrente[] novoArray = new ContaCorrente[capacity];
+
+            Console.WriteLine($"Aumentando capacidade do array");
+
+            for (var i = 0; i <= _items.Length - 1; i++)
+            {
+                novoArray[i] = _items[i];
+            }
+
+            _items = novoArray;
+
+        }
+    }
+}
--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -7,10 +7,57 @@ namespace ByteBank.SistemaAgencia
     {
         public static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(222, 2222222);
+            ListaContaCorrente lista = new ListaContaCorrente();
 
-            Console.WriteLine(conta.Numero);
+            lista.Adicionar(new ContaCorrente(12345, 1234567890));
+            lista.Adicionar(new ContaCorrente(12345, 1234567890));
+            lista.Adicionar(new ContaCorrente(12345, 1234567890));
+
+
             Console.ReadLine();
+        }
+
+        static void TestaContaArray()
+        {
+            ContaCorrente [] contas = new ContaCorrente[]
+                {
+                    new ContaCorrente(12345, 1234567890),
+                    new ContaCorrente(1234, 12567890),
+                    new ContaCorrente(1345, 1237890)
+                };
+
+            for (var i = 0; i <= contas.Length - 1; i++)
+            {
+                Console.WriteLine($"Conta {i} {contas[i].Numero}");
+            }
+        }
+
+        static void TestaArrayInt()
+        {
+            int[] idades = new int[6];
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            //idades[3] = 50;
+            //idades[4] = 28;
+            //idades[5] = 10;
+
+            var idadeNoIndice4 = idades[4];
+
+            Console.WriteLine(idadeNoIndice4);
+
+            var acc = 0;
+
+            for (int i = 0; i <= idades.Length - 1; i++)
+            {
+                var idade = idades[i];
+                Console.WriteLine($"Acessando idade no indice {i}, idade: {idade}");
+                acc += idade;
+            }
+
+            var average = acc / idades.Length;
+
+            Console.WriteLine($"Media de idades == {average}");
         }
     }
 }
