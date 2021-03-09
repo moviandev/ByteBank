51dfd99e290338750e2b348617500d52e8671e88
Matheus Viana <matheus_o_viana@hotmail.com>
2021-03-09 19:44:52 -0300

    added string parsing

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -20,10 +20,34 @@ namespace ByteBank.SistemaAgencia
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
-                    Console.WriteLine(line);
+                    var cc = ConvertStringToContaCorrente(line);
+
+                    Console.WriteLine($"Account: {cc.Numero}, Agency: {cc.Agencia}, Costumer: {cc.Titular.Nome}, Limit: {cc.Saldo}");
                 }
 
             Console.ReadLine();
+        }
+
+        public static ContaCorrente ConvertStringToContaCorrente(string line)
+        {
+            var fields = line.Split(' ');
+
+            var agency = fields[0];
+            var account = fields[1];
+            var limit = fields[2].Replace('.', ',');
+            var titualr = fields[3];
+
+            var intAgency = int.Parse(agency);
+            var intAccount = int.Parse(account);
+            var doubleLimit = double.Parse(limit);
+
+            var costumer = new Cliente();
+            costumer.Nome = titualr;
+            var result = new ContaCorrente(intAgency, intAccount);
+            result.Depositar(doubleLimit);
+            result.Titular = costumer;
+
+            return result;
         }
 
         public static void TestaLinq()
